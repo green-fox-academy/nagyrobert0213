19cdab7969b9497a4b00347c8d66e7cdad559a1a
nagyrobert0213 <nagyrobert0213@gmail.com>
2018-08-02 13:05:24 +0200

    added introduceyourself

---
+++ week-01/day-03/IntroduceYourself/src/IntroduceYourself.java
@@ -0,0 +1,7 @@
+public class IntroduceYourself {
+    public static void main(String[] args){
+        System.out.println("Nagy Robert");
+        System.out.println("21" );
+        System.out.println("1.80");
+    }
+}
