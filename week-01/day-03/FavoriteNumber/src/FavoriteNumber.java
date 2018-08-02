bd851c928ab49c0204acc8b2d961c1f92bb05382
nagyrobert0213 <nagyrobert0213@gmail.com>
2018-08-02 13:50:41 +0200

    added favoritenumber

---
+++ week-01/day-03/FavoriteNumber/src/FavoriteNumber.java
@@ -0,0 +1,6 @@
+public class FavoriteNumber {
+    public static void main(String[] args){
+        int favoriteNumber = 13;
+        System.out.println("Your favorite number is " + favoriteNumber +   "!");
+    }
+}
