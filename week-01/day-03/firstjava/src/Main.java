328442fd93a026a923192f03ae20a5d4cd8e88a2
nagyrobert0213 <nagyrobert0213@gmail.com>
2018-08-02 11:58:15 +0200

    added hellome

--- week-01/day-03/firstjava/src/Main.java
+++ week-01/day-03/firstjava/src/Main.java
@@ -1,4 +1,38 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class Main {
+    public static void main(String[] args){
+        /* int intNum = 0;
+        intNum++;
+        intNum += 5;
+        double dub = 1.5;
+        boolean bool = true;
+        String name = "Robi";
+        char firstChar = 'c';
+        System.out.println("Szia " + name);
+        */
+        /* int number = 15;
+        if(number % 3 == 0 && number % 5 == 0){
+            System.out.println("I can be divided by 3 and 5."); */
+
+        /* int i = 0;
+        while(i < 10){
+            System.out.println("BADBOI is awesome!");
+            i++; */
+        /* for(int i = 0; i < 10; i++){
+            System.out.println("gf");
+        }
+        int j = 0;
+        do{
+            System.out.println("hello");
+            j++;
+        }while(j != 5); */
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Add meg a neved!");
+        String line = sc.nextLine();
+        int number = sc.nextInt();
+        System.out.println("Hello " + line + "!");
+        System.out.println(number);
+    }
 }
