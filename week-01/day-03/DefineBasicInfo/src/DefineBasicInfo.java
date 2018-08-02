e078704fc6f8ddca5887efcf0ee4bc42032e1f07
nagyrobert0213 <nagyrobert0213@gmail.com>
2018-08-02 14:47:31 +0200

    add definebasicinfo

---
+++ week-01/day-03/DefineBasicInfo/src/DefineBasicInfo.java
@@ -0,0 +1,12 @@
+public class DefineBasicInfo {
+    public static void main (String[] args){
+        String name = "Robert Nagy";
+        int age = 21;
+        double heightInMetre = 1.80;
+        boolean married = false;
+        System.out.println(name);
+        System.out.println(age);
+        System.out.println(heightInMetre);
+        System.out.println(married);
+    }
+}
