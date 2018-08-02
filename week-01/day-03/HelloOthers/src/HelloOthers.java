c86d18ce18fff27785cb158e8ad1dc41a0b41bac
nagyrobert0213 <nagyrobert0213@gmail.com>
2018-08-02 13:01:39 +0200

    added helloothers

---
+++ week-01/day-03/HelloOthers/src/HelloOthers.java
@@ -0,0 +1,7 @@
+public class HelloOthers {
+    public static void main(String[] args){
+        System.out.println("Hello, Csilla");
+        System.out.println("Hello, Robi");
+        System.out.println("Hello, Dani");
+    }
+}
