645f9300a3022812e29c010e417020a3d13940ea
nagyrobert0213 <nagyrobert0213@gmail.com>
2018-08-03 12:15:21 +0200

    add milestokm

--- week-01/day-03/MileToKmConverter/src/MileToKmConverter.java
+++ week-01/day-03/MileToKmConverter/src/MileToKmConverter.java
@@ -3,8 +3,12 @@ import java.util.Scanner;
 public class MileToKmConverter {
     public static void main(String[] args) {
 
-        int askKm;
-        askKm = kb  .nextLine();
+        System.out.println("Please give me a distance in miles!");
+        Scanner scanner = new Scanner(System.in);
+        int askKm = scanner.nextInt();
+        System.out.println(askKm);
+        double resultInMiles = (double) askKm * 1.609344;
+        System.out.format("%.3f%n",resultInMiles); System.out.println("KMs");
 
     }
 }
