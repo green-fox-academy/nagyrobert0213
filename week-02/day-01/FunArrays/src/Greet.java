164b3887712e7df9b872ad847f9b47086c067fc5
nagyrobert0213 <nagyrobert0213@gmail.com>
2018-08-06 15:57:58 +0200

    feat(greet): add greet

---
+++ week-02/day-01/FunArrays/src/Greet.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Greet {
+    public static void main(String[] args) {
+        String a1 = "Greenfox";
+        greet(a1);
+    }
+
+    public static void greet(String grtNum1){
+        System.out.println("Greetings, dear \n" + grtNum1);
+    }
+}
