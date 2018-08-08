ae97fabc16a958ed03b507b3407af97916bc68c8
nagyrobert0213 <nagyrobert0213@gmail.com>
2018-08-08 16:27:59 +0200

    feat(postitionsquare): add positionsquare

--- week-02/day-03/PositionSquare/src/PositionSquare.java
+++ week-02/day-03/PositionSquare/src/PositionSquare.java
@@ -1,40 +1,51 @@
 import javax.swing.*;
 
 import java.awt.*;
-import java.util.ArrayList;
 import java.util.Random;
 
 import static javax.swing.JFrame.EXIT_ON_CLOSE;
 
-public class FourRectangles {
+public class PositionSquare{
 
     public static void mainDraw(Graphics graphics){
-        // draw four different size and color rectangles.
+        // create a square drawing function that takes 2 parameters:
+        // the x and y coordinates of the square's top left corner
+        // and draws a 50x50 square from that point.
+        // draw 3 squares with that function.
         // avoid code duplication.
 
+
         Random rnd = new Random();
+        for(int i = 0; i < 3; i++){
+            int x = rnd.nextInt(200);
+            int y = rnd.nextInt(200);
 
-        for(int i = 0; i < 4; i++){
-            int h = rnd.nextInt(30);
-            int j = rnd.nextInt(40);
-            int k = rnd.nextInt(10);
-            int l = rnd.nextInt(330);
-
-            rectangleOne(graphics,h,j,k,l);
+            squareDrawing(graphics,x,y);
         }
+        /*squareDrawing(graphics,x1,y1);
+
+        int x2 = 20;
+        int y2 = 20;
+
+        squareDrawing(graphics, x2, y2);
+
+        int x3 = 30;
+        int y3 = 30;
+        squareDrawing(graphics, x3, y3);
+*/
+
+
     }
 
-    public static void rectangleOne(Graphics graphics, int h, int j, int k, int l){
-        graphics.drawRect(h, j, k, l);
+    public static void squareDrawing(Graphics graphics, int x, int y){
+        graphics.drawRect(x,y,50,50);
+
     }
-
-
-
-
 
     //    Don't touch the code below
     static int WIDTH = 320;
     static int HEIGHT = 343;
+
     public static void main(String[] args) {
         JFrame jFrame = new JFrame("Drawing");
         jFrame.setSize(new Dimension(WIDTH, HEIGHT));
