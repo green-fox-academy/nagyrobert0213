187917e71abe162e9f0fa45590ba521604c8e681
nagyrobert0213 <nagyrobert0213@gmail.com>
2018-08-08 13:52:33 +0200

    feat (diagonals) add diagonals

---
+++ week-02/day-03/GoToCenter/src/GoToCenter.java
@@ -0,0 +1,53 @@
+import javax.swing.*;
+
+import java.awt.*;
+import java.util.Random;
+
+import static javax.swing.JFrame.EXIT_ON_CLOSE;
+
+public class GoToCenter {
+    public static void mainDraw(Graphics graphics) {
+        // Create a line drawing function that takes 2 parameters:
+        // The x and y coordinates of the line's starting point
+        // and draws a line from that point to the center of the canvas.
+        // Draw 3 lines with that function. Use loop for that.
+
+
+
+        int numberOfLines = 3;
+
+        for (int i = 0; i < numberOfLines; i++){
+            drawLineFunction(graphics, (int) (20 + Math.random() * WIDTH), (int) (20 + Math.random() * HEIGHT));
+        }
+    }
+
+
+
+    public static void drawLineFunction(Graphics graphics, int x, int y){
+
+        graphics.drawLine(x,y,160,343/2);
+
+
+    }
+
+    // Don't touch the code below
+    static int WIDTH = 320;
+    static int HEIGHT = 343;
+
+    public static void main(String[] args) {
+        JFrame jFrame = new JFrame("Drawing");
+        jFrame.setSize(new Dimension(WIDTH, HEIGHT));
+        jFrame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        jFrame.add(new ImagePanel());
+        jFrame.setLocationRelativeTo(null);
+        jFrame.setVisible(true);
+    }
+
+    static class ImagePanel extends JPanel {
+        @Override
+        protected void paintComponent(Graphics graphics) {
+            super.paintComponent(graphics);
+            mainDraw(graphics);
+        }
+    }
+}
