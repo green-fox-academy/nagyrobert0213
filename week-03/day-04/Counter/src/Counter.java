4271bcd40407d54d94b89b53849f0cdea812e22c
nagyrobert0213 <nagyrobert0213@gmail.com>
2018-08-16 15:00:20 +0200

    feat(counter): add task but still shit

---
+++ week-03/day-04/Counter/src/Counter.java
@@ -0,0 +1,26 @@
+    public class Counter{
+
+    /*Create Counter class
+    which has an integer field value
+    when creating it should have a default value 0 or we can specify it when creating
+    we can add(number) to this counter another whole number
+    or we can add() without parameters just increasing the counter's value by one
+    and we can get() the current value
+    also we can reset() the value to the initial value*/
+
+    int value = 0;
+
+    public int add(int a){
+        a = value++;
+        return a;
+    }
+    public int get(){
+        System.out.println(value);
+        return value;
+    }
+    public void reset(){
+        value = 0;
+        System.out.println("Value reseted.");
+    }
+
+    }
---
+++ week-03/day-04/Counter/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        Counter cnt = new Counter();
+        cnt.add();
+        cnt.add();
+        cnt.get();
+        cnt.reset();
+    }
+}
