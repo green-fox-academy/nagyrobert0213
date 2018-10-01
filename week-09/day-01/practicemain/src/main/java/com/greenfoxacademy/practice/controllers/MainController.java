5996dc8d48b25993950e7b1e7f2f32a43d168a3f
nagyrobert0213 <nagyrobert0213@gmail.com>
2018-10-01 13:35:19 +0200

    feat(task1): started task

---
+++ week-09/day-01/practicemain/src/main/java/com/greenfoxacademy/practice/controllers/MainController.java
@@ -0,0 +1,13 @@
+package com.greenfoxacademy.practice.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+
+    @GetMapping("/")
+    public String indexGet(){
+        return "index";
+    }
+}
